4bab2f1eee2650663972c03dc9d77eae210adc10
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-05-09 23:16:53 +0430

    v1.2

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -1,5 +1,5 @@
 <?php
-define('SITE_TITLE','7Task Project');
-define('BASE_URL','http://localhost/7Learn.php/7Todo/');
-define('BASE_PATH','C:/xampp/htdocs/7Learn.php/7Todo/');
+define('SITE_TITLE','ToDo Project');
+define('BASE_URL','http://localhost/ToDo/');
+define('BASE_PATH','C:/xampp/htdocs/ToDo/');
 
